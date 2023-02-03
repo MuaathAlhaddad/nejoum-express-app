4e47c329fa5c23c9cbf2ef3cf64ae6f1ad1cfe58
muaadh <muaath2000@gmail.com>
2023-02-03 23:41:34 +0800

    chore: change location map

--- resources/views/components/about/location.blade.php
+++ resources/views/components/about/location.blade.php
@@ -12,8 +12,7 @@
             <div class="gmap-outline mb-5">
                 <div class="google-map">
                     <div style="width: 100%">
-                        <iframe height="460"
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3304.8534521658976!2d-118.2533646842856!3d34.073270780600225!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c6fd9829c6f3%3A0x6ecd11bcf4b0c23a!2s1363%20Sunset%20Blvd%2C%20Los%20Angeles%2C%20CA%2090026%2C%20USA!5e0!3m2!1sen!2sin!4v1620815366832!5m2!1sen!2sin"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13865.07150080026!2d-95.3270179!3d29.6830124!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2de73fda07788a94!2sNejoum%20Express%20Used%20cars%20INC!5e0!3m2!1sen!2smy!4v1675438845408!5m2!1sen!2smy" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
             </div>
