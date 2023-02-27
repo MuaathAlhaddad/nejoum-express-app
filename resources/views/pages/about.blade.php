b6c3913cce1ac8b8273ba573d3daa435f26cb332
muaadh <muaath2000@gmail.com>
2023-02-28 00:47:48 +0800

    fix(about): Swap mission and vision

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -7,9 +7,9 @@
 
         @include('components.hero-section', ['title' => 'About Us', 'breadcrumb' => 'About Us', 'banner' => 'about-page.png'])
 
-        @include('components.about.mission-vision', ['title' => 'Mission', 'content' => 'Upgrading the trading field of exported used cars through keeping up with the newest technology integrating it in our company. In addition, the expansion to other countries through initiating other branches and enhancing our level of service while still maintaining competitiveness in the market.'])
-
         @include('components.about.mission-vision', ['title' => 'Vision', 'content' => 'Easing the vehicle Shipping and making it accessible around the USA'])
+
+        @include('components.about.mission-vision', ['title' => 'Mission', 'content' => 'Upgrading the trading field of exported used cars through keeping up with the newest technology integrating it in our company. In addition, the expansion to other countries through initiating other branches and enhancing our level of service while still maintaining competitiveness in the market.'])
 
         @include('components.about.what-we-do')
 
